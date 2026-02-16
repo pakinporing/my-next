95069940979b9f628a1d5d032d0f428afc3c1123
pakinporing <pakinporing@gmail.com>
2026-02-16 11:50:45 +0700

    testtssdsfsdf

--- libs/db/prisma.ts
+++ libs/db/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@/libs/db/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from '@/libs/db/generated/prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// export const prisma = new PrismaClient({ adapter });
 
 // const globalForPrisma = global as unknown as {
 //   prisma: PrismaClient;
